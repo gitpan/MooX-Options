c638a16773003b52214645b6e00aa536c759467a
celogeek <me@celogeek.com>
2012-12-08 19:08:00 +0000

    Import of CELOGEEK/MooX-Options-3.73 from CPAN.
    gitpan-cpan-distribution: MooX-Options
    gitpan-cpan-version:      3.73
    gitpan-cpan-path:         CELOGEEK/MooX-Options-3.73.tar.gz
    gitpan-cpan-author:       CELOGEEK
    gitpan-cpan-maturity:     released

--- lib/MooX/Options/Role.pm
+++ lib/MooX/Options/Role.pm
@@ -12,7 +12,7 @@ package MooX::Options::Role;
 use strict;
 use warnings;
 
-our $VERSION = '3.72';    # VERSION
+our $VERSION = '3.73';    # VERSION
 
 use MRO::Compat;
 use Moo::Role;
@@ -149,7 +149,7 @@ MooX::Options::Role - role that is apply to your object
 
 =head1 VERSION
 
-version 3.72
+version 3.73
 
 =head1 METHODS
 
