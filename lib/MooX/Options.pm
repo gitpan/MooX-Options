35667c94385ebffc9def5aa6f58ca4629de5e6fb
celogeek <me@celogeek.com>
2013-10-14 20:32:21 +0000

    Import of CELOGEEK/MooX-Options-3.90 from CPAN.
    gitpan-cpan-distribution: MooX-Options
    gitpan-cpan-version:      3.90
    gitpan-cpan-path:         CELOGEEK/MooX-Options-3.90.tar.gz
    gitpan-cpan-author:       CELOGEEK
    gitpan-cpan-maturity:     released

--- lib/MooX/Options.pm
+++ lib/MooX/Options.pm
@@ -14,7 +14,7 @@ use strict;
 use warnings;
 use Carp;
 
-our $VERSION = '3.89';    # VERSION
+our $VERSION = '3.90';    # VERSION
 my @OPTIONS_ATTRIBUTES
     = qw/format short repeatable negativable autosplit doc order json/;
 
@@ -188,7 +188,7 @@ MooX::Options - add option keywords to your object (Mo/Moo/Moose)
 
 =head1 VERSION
 
-version 3.89
+version 3.90
 
 =head1 MooX::Options
 
